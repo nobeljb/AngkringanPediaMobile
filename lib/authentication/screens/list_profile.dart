5be437d0c2e9b724b5d2e526235f75bec8504bb1
kaachiii <kaachi579@gmail.com>
2024-12-18 06:45:46 +0700

    update list profile

--- lib/authentication/screens/list_profile.dart
+++ lib/authentication/screens/list_profile.dart
@@ -1,6 +1,7 @@
 import 'package:angkringan_pedia/authentication/widgets/left_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:angkringan_pedia/authentication/models/profile.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'profile_detail.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
@@ -14,12 +15,34 @@ class ListProfilePage extends StatefulWidget {
 
 class _ListProfilePageState extends State<ListProfilePage> {
   Future<List<Profile>> fetchProfiles(CookieRequest request) async {
-    final response = await request.get('http://127.0.0.1:8000/authentication/json/');
+    // Ambil data JSON dari server
+    final response =
+        await request.get('http://127.0.0.1:8000/authentication/json/');
     List<Profile> profiles = [];
 
+    // Inisialisasi FlutterSecureStorage
+    final storage = FlutterSecureStorage();
+
+    // Ambil id user dari FlutterSecureStorage dan konversi ke int
+    final String? idString = await storage.read(key: 'id');
+    final int? userId = idString != null ? int.tryParse(idString) : null;
+
+    // print("user id: $userId");
+
+    // if (userId == null) {
+    //   print('Error: User ID not found or invalid.');
+    //   return profiles; // Kembalikan daftar kosong jika ID tidak ditemukan
+    // }
+
+    // Iterasi data JSON dan tambahkan profil yang bukan milik user
     for (var data in response) {
-      profiles.add(Profile.fromJson(data));
+      final profile = Profile.fromJson(data);
+      if (profile.fields.user != userId) {
+        // print("profile.pk ${profile.pk}");
+        profiles.add(profile);
+      }
     }
+
     return profiles;
   }
 
@@ -39,13 +62,15 @@ class _ListProfilePageState extends State<ListProfilePage> {
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('Belum ada data user pada Angkringan Pedia.'));
+            return const Center(
+                child: Text('Belum ada data user pada Angkringan Pedia.'));
           } else {
             return ListView.builder(
               itemCount: snapshot.data.length,
               itemBuilder: (context, index) {
                 final profile = snapshot.data[index];
-                final imageUrl = "http://127.0.0.1:8000/${profile.fields.profileImage}"; 
+                final imageUrl =
+                    "http://127.0.0.1:8000/${profile.fields.profileImage}";
                 return ListTile(
                   leading: CircleAvatar(
                     backgroundImage: NetworkImage(imageUrl),
@@ -55,7 +80,8 @@ class _ListProfilePageState extends State<ListProfilePage> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ProfileDetailPage(profile: profile),
+                        builder: (context) =>
+                            ProfileDetailPage(profile: profile),
                       ),
                     );
                   },
